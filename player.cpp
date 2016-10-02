1d2b16760012d3965ed733dd3e9ff4329222412d
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2016-10-02 11:46:54 +0530

    generate moves for either players

--- player.cpp
+++ player.cpp
@@ -16,6 +16,10 @@ typedef int8 Point;
 typedef string Move;
 typedef vector<Move> Moves;
 
+inline pair<int, int> make_xy(char x, char y) {
+    return make_pair(x-'a', y-1);
+}
+
 enum Stones {
     BLACK_FLAT,
     BLACK_WALL,
@@ -31,9 +35,11 @@ class Board {
 
 public:
     vector<Stones> board[5][5];
-    int flats_rem = 21;
-    int caps_rem = 1;
-    
+    int white_flats_rem = 21;
+    int white_caps_rem = 1;
+    int black_flats_rem = 21;
+    int black_caps_rem = 1;
+
     bool empty(int x, int y) {
         assert(0 <= x and x < N);
         assert(0 <= y and y < N);
@@ -58,15 +64,41 @@ public:
     bool black_cap(int x, int y) {
         return (not empty(x, y) and board[x][y].back() == BLACK_CAP);
     }
-
+    bool white(int x, int y) {
+        return white_flat(x, y) || white_wall(x, y) || white_cap(x, y);
+    }
+    bool black(int x, int y) {
+        return black_flat(x, y) || black_wall(x, y) || black_cap(x, y);
+    }
     int height(int x, int y) {
         return board[x][y].size();
     }
+
+    void perform_placement(Move move, bool white) {
+        const pair<int, int> xy = make_xy(move[1], move[2]);
+        const int x = xy.first; 
+        const int y = xy.second;
+        if(white) {
+            switch(move[0]) {
+                case 'F': board[x][y].push_back(WHITE_FLAT); black_flats_rem--; break;
+                case 'W': board[x][y].push_back(WHITE_WALL); black_flats_rem--; break;
+                case 'C': board[x][y].push_back(WHITE_CAP);  black_caps_rem--; break;
+                default : assert(false);
+            }
+        } else {
+            switch(move[0]) {
+                case 'F': board[x][y].push_back(BLACK_FLAT); black_flats_rem--; break;
+                case 'W': board[x][y].push_back(BLACK_WALL); black_flats_rem--; break;
+                case 'C': board[x][y].push_back(BLACK_CAP);  black_caps_rem--; break;
+                default : assert(false);
+            }
+        }
+    }
 };
 
-void generate_moves(Board &board);
-void generate_placement_moves(Board &board, Moves &moves);
-void generate_motion_moves(Board &board, Moves &moves);
+void generate_moves(Board &board, bool white);
+void generate_placement_moves(Board &board, Moves &moves, bool white);
+void generate_motion_moves(Board &board, Moves &moves, bool white);
 string make_sqr(int x, int y);
 void motion(const char dir, int x, int y, string prefix, Board &board, Moves &moves, int ht);
 void cap_motion(const char dir, int x, int y, string prefix, Board &board, Moves &moves, int ht);
@@ -80,14 +112,18 @@ inline string make_sqr(int x, int y) {
     return s;
 }
 
+
 inline bool out_of_bounds(int x, int y) {
     return not(0 <= x and x < N and 0 <= y and y < N);
 }
 
-void generate_moves(Board &board) {
+void generate_moves(Board &board, bool white) {
+    /* generates a list of moves for either player 
+       and prints them out 
+    */
     vector<Move> moves;
-    // generate_placement_moves(board, moves);
-    generate_motion_moves(board, moves);
+    // generate_placement_moves(board, moves, white);
+    generate_motion_moves(board, moves, white);
     cout << "[";
     for(string move : moves) {
         cout << move << ", ";
@@ -96,16 +132,19 @@ void generate_moves(Board &board) {
 }
 
 
-void generate_placement_moves(Board &board, Moves &moves) {
+void generate_placement_moves(Board &board, Moves &moves, bool white) {
+    /* generates moves to place pieces for white and black */
+    const int flats = (white) ? board.white_flats_rem : board.black_flats_rem;
+    const int caps = (white) ? board.white_caps_rem : board.black_caps_rem;
     for(int x = 0; x < N; ++x) {
         for(int y = 0; y < N; ++y) {
             if(board.empty(x, y)) {
                 string s = make_sqr(x, y);
-                if(board.flats_rem > 0) {
+                if(flats > 0) {
                     moves.push_back("W" + s);
                     moves.push_back("F" + s);
                 }
-                if(board.caps_rem > 0) {
+                if(caps > 0) {
                     moves.push_back("C" + s);
                 }
             }
@@ -132,27 +171,30 @@ inline int next_y(int y, char dir) {
         default: assert(false);
     }
 }
-void generate_motion_moves(Board &board, Moves &moves) {
+void generate_motion_moves(Board &board, Moves &moves, bool white) {
+    /* generate the moves for moving stacks for either player */
     const string dirs = "+-<>";
-
-    for(int dir=0; dir < 4; ++dir) {
+    for(int dir = 0; dir < 4; ++dir) {
         for(int x = 0; x < N; ++x) {
             for(int y = 0; y < N; ++y) {
                 if(board.empty(x, y)) continue;
-                int xx = next_x(x, dirs[dir]);
-                int yy = next_y(y, dirs[dir]);
-                if(board.white_flat(x, y) || board.white_wall(x, y)) {
-                    int H = board.height(x, y);
-                    for(int h = 1; h <= min(H, N); ++h) {
-                        string prefix = to_string(h) + make_sqr(x, y) + dirs[dir];
+                const int xx = next_x(x, dirs[dir]);
+                const int yy = next_y(y, dirs[dir]);
+                /* some predicates for testing the exitence of players' stack */
+                const bool white_cap = (white and board.white_cap(x, y));
+                const bool black_cap = ((not white) and board.black_cap(x, y));
+                const bool white_stack = (white and (board.white_flat(x, y) or board.white_wall(x, y)));
+                const bool black_stack = ((not white) and ((board.black_flat(x, y) or board.black_wall(x, y))));
+                const int H = board.height(x, y);
+                /* because there is a carry limit */
+                for(int h = 1; h <= min(H, N); ++h) {
+                    string prefix = to_string(h) + make_sqr(x, y) + dirs[dir];
+                    if(white_stack or black_stack) {
+                        /* there is a stack for the player which he might move */
                         motion(dirs[dir], xx, yy, prefix, board, moves, h);
                     }
-                    cout << "works\n";
-                }
-                else if(board.white_cap(x, y)) {
-                    int H = board.height(x, y);
-                    for(int h = 1; h <= min(H, N); ++h) {
-                        string prefix = to_string(h) + make_sqr(x, y) + dirs[dir];
+                    else if(white_cap or black_cap) {
+                        /* there is a capstone stack for the player which he might move */
                         cap_motion(dirs[dir], xx, yy, prefix, board, moves, h);
                     }
                 }
@@ -185,9 +227,16 @@ void cap_motion(const char dir, int x, int y, string prefix, Board &board, Moves
     }
 }
 
+void perform_move(Board &board, const Move move, bool white=true) {
+    if(move[0] == 'F' || move[0] == 'S' || move[0] == 'C') 
+        board.perform_placement(move, white);
+    else ; //perform_motion(move, white);
+}
+
 int main() {
     Board board;
-    board.board[0][0].push_back(WHITE_CAP);
-    board.board[1][0].push_back(BLACK_WALL);
-    generate_moves(board);
+    board.board[0][0].push_back(WHITE_WALL);
+    board.board[1][0].push_back(BLACK_CAP);
+    generate_moves(board, false);
+
 }
